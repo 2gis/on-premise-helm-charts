10badeb3eff564fadb09a8127bb7a3c9cd1422a7
Сиденко Кирилл Сергеевич <k.sidenko@2gis.ru>
2022-03-21 10:43:34 +0700

    cr fix

--- charts/gis-platform/gis-platform-config/configure.sh
+++ charts/gis-platform/gis-platform-config/configure.sh
@@ -76,9 +76,9 @@ echo "Share all basemaps: $layer_names"
 $CURL -XPOST -H 'Content-Type: application/json-patch+json' -d "$layer_names" "$GIS_PLATFORM_URL/sp/resources/layers/shareAll"
 
 echo "Setting autoshared list for all basemaps"
-for layer in layer/*json; do
+for layer in layer/*.json; do
     layer_name=$(jq --raw-output .name $layer)
-    data="{\"name\":\"${layer_name}\", \"type\": \"layer\"}"
+    data=$(jq --compact-output '{name: .name, type: "layer"}' $layer)
     $CURL -XPOST -H 'Content-Type: application/json-patch+json' -d "$data" "$GIS_PLATFORM_URL/sp/autosharedList"
 done
 
