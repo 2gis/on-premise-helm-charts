7a5076f6d7cf22e24135a0ec9863c10f5d508fbb
Kirill Sidenko <k.sidenko@2gis.ru>
2022-03-21 10:58:35 +0700

    fix sharing with base layers for new created user (#37)
    * fix sharing with base layers for new created user

--- charts/gis-platform/gis-platform-config/configure.sh
+++ charts/gis-platform/gis-platform-config/configure.sh
@@ -75,4 +75,10 @@ layer_names=$(jq --slurp --compact-output '[ .[].name ]' layer/*.json)
 echo "Share all basemaps: $layer_names"
 $CURL -XPOST -H 'Content-Type: application/json-patch+json' -d "$layer_names" "$GIS_PLATFORM_URL/sp/resources/layers/shareAll"
 
+echo "Setting autoshared list for all basemaps"
+for layer in layer/*.json; do
+    data=$(jq --compact-output '{name: .name, type: "layer"}' $layer)
+    $CURL -XPOST -H 'Content-Type: application/json-patch+json' -d "$data" "$GIS_PLATFORM_URL/sp/autosharedList"
+done
+
 rm $cookie
