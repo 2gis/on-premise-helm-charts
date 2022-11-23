2d27876c37025c715f8c977ed5f89a72eb10eefc
Denis Belyaev <74010851+dbelyaev-nsk@users.noreply.github.com>
2022-11-23 14:33:35 +0700

    release-1.4.5 (#191)
    * release-1.4.5
    
    * upd helmfile

--- helmfile/tests/navi-async-service.sh
+++ helmfile/tests/navi-async-service.sh
@@ -13,14 +13,25 @@ cd $SCRIPTPATH/points
 TASK_ID=`curl -s $URL/create_task/get_dist_matrix?key=$KEY --header 'Content-Type: application/json' -d @moscow_dm.json | jq -r '.task_id'`
 echo "Start test Moscow"
 echo "Create task with ID" $TASK_ID
-
+sleep 5
 function task_status() {
   echo `curl -s $URL/result/get_dist_matrix/$TASK_ID?key=$KEY | jq -r '.status'`
 }
 
 function task_response() {
-  LINK=`curl -s $URL/result/get_dist_matrix/$TASK_ID?key=$KEY | jq -r '.result_link'`
-  curl -s $LINK
+  LINK=`curl -s https://$IHOST/result/get_dist_matrix/$TASK_ID?key=$KEY | jq -r '.result_link'`
+  if [[ $LINK == "" ]] ;then
+    echo "response.json not found on s3, check settings s3 on navi-back-async" && exit 1
+  else
+    response=`curl -s $LINK`
+    echo -e $response $'\n'
+    response_status=`echo $response | jq .routes[].status | grep OK | wc -l`
+    if [[ $response_status -ge "1" ]]; then
+      echo "OK"
+    else
+      echo "No successful routes" && exit 1
+    fi
+  fi
 }
 
 echo "Task status" `task_status`
@@ -31,9 +42,9 @@ while [[ `task_status` != "TASK_DONE" && "$i" -lt "5" ]]; do
 done
 
 if [[ "$i" -ge "5" ]] || [[ `task_status` != "TASK_DONE" ]];then
-  echo "Task status" task_status
-  echo "long task processing, check logs navi-async-service and navi-back-async" && exit 1
+  echo "Task status" `task_status`
+  echo "Long task processing, check logs navi-async-service and navi-back-async" && exit 1
 elif [[ `task_status` == "TASK_DONE" ]];then
   echo "Task status" `task_status`
-  echo `task_response`
+  task_response
 fi
