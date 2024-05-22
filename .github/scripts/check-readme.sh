685fe059d51d1e5b701e9f999b83f53894b91b9e
Dmitrii Moskalev <d.moskalev@2gis.ru>
2024-05-22 09:43:45 +0300

    update

--- .github/scripts/check-readme.sh
+++ .github/scripts/check-readme.sh
@@ -4,13 +4,28 @@ set -e
 set -u
 set -o pipefail
 
+# Определяем директории
 SCRIPT_DIR="$( cd $(dirname "$0") ; pwd )"
 REPO_PATH=$( git -C "$SCRIPT_DIR" rev-parse --show-toplevel )
 
 cd "$REPO_PATH"
 
-readme-generator --config=/config.json --values=/values.yaml --readme=/README.md
+# Проверка наличия утилиты readme-generator
+if ! command -v readme-generator &> /dev/null; then
+  echo "readme-generator could not be found, please install it."
+  exit 1
+fi
 
+# Запуск генератора для каждой поддиректории в charts
+for chart in charts/*; do
+  if [ -d "$chart" ]; then
+    echo "Building README for $chart..."
+    readme-generator --config="$REPO_PATH/bitnami-config.json" --values="$REPO_PATH/$chart/values.yaml" --readme="$REPO_PATH/$chart/README.md"
+    echo ""
+  fi
+done
+
+# Проверка, есть ли несохраненные изменения в репозитории
 IS_DIRTY=0
 HAS_UNTRACKED=0
 
@@ -21,12 +36,12 @@ git -C "$REPO_PATH" ls-files --others --exclude-standard --directory --no-empty-
 RESULT=$(( IS_DIRTY + HAS_UNTRACKED ))
 
 if [[ "$RESULT" -eq 0 ]]; then
-    echo 'Documentation is up-to-date'
+  echo 'Documentation is up-to-date'
 else
-    echo 'You need to update documetation: run `make prepare && make all`'
-    echo 'Changed files:'
-    git status --porcelain
-    exit 1
+  echo 'You need to update documentation: run `make prepare && make all`'
+  echo 'Changed files:'
+  git status --porcelain
+  exit 1
 fi
 
 exit 0
